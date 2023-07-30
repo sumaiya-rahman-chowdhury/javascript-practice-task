dca62361f217adfe66164f23fa1ece3085b7f1fc
Sumaiya-rahman-chowdhury <sumaiyaprionty@gmail.com>
2023-07-30 06:59:47 -0700

    first commit

---
+++ js array/app.js
@@ -0,0 +1,8 @@
+
+var fruits = ['Apple','Banana','Orange'];
+var element = fruits.indexOf('Banana');
+// element = ;
+fruits[1] = 'Mango';
+fruits.pop();
+fruits.push("WaterMelon");
+document.getElementById("demo").innerHTML = (fruits);
